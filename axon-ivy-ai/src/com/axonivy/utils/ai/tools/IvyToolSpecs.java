2fa7e48b250bb171945f386320c84deb6a33a41f
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-07-31 07:21:53 +0200

    AIAI-70 parameters for complex tool inputs

--- axon-ivy-ai/src/com/axonivy/utils/ai/tools/IvyToolSpecs.java
+++ axon-ivy-ai/src/com/axonivy/utils/ai/tools/IvyToolSpecs.java
@@ -7,10 +7,8 @@ import org.apache.commons.lang3.StringUtils;
 import ch.ivyteam.ivy.application.IProcessModelVersion;
 import ch.ivyteam.ivy.process.model.element.event.start.CallSubStart;
 import ch.ivyteam.ivy.process.model.element.event.start.value.CallSignature;
-import ch.ivyteam.ivy.process.model.value.scripting.VariableDesc;
 import dev.langchain4j.agent.tool.ToolSpecification;
 import dev.langchain4j.agent.tool.ToolSpecification.Builder;
-import dev.langchain4j.model.chat.request.json.JsonObjectSchema;
 
 @SuppressWarnings("restriction")
 public class IvyToolSpecs {
@@ -29,31 +27,12 @@ public class IvyToolSpecs {
     if (StringUtils.isNotBlank(toolStart.getDescription())) {
       builder.description(toolStart.getDescription());
     }
-    var params = JsonObjectSchema.builder();
-    signature.getInputParameters().stream()
-        .forEach(variable -> toJsonParam(variable, params));
-    builder.parameters(params.build());
+
+    var params = new JsonToolParamBuilder(IProcessModelVersion.current())
+        .toParams(signature.getInputParameters());
+    builder.parameters(params);
 
     return builder.build();
   }
 
-  private static void toJsonParam(VariableDesc variable, JsonObjectSchema.Builder builder) {
-    switch (variable.getType().getSimpleName()) {
-      case "String":
-        builder.addStringProperty(variable.getName(), variable.getInfo().getDescription());
-        break;
-      case "Integer":
-        builder.addIntegerProperty(variable.getName(), variable.getInfo().getDescription());
-        break;
-      case "Number":
-        builder.addNumberProperty(variable.getName(), variable.getInfo().getDescription());
-        break;
-      case "Boolean":
-        builder.addBooleanProperty(variable.getName(), variable.getInfo().getDescription());
-        break;
-      default:
-        // TODO native schema builder! see https://github.com/langchain4j/langchain4j/pull/3374
-        break;
-    }
-  }
 }
