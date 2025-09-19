162f3149fadb94e313db59c15d57ca6ff2766d12
mnhnam-ivy <nam.mai@axonactive.com>
2025-09-19 13:53:55 +0700

    AIAI-148 Prepare Market Place Connector Release for Smart Orchstrator Alpha - Fixed failed test

--- smart-orchestrator-test/src_test/com/axonivy/utils/smart/orchestrator/demo/support/mock/SupportToolChat.java
+++ smart-orchestrator-test/src_test/com/axonivy/utils/smart/orchestrator/demo/support/mock/SupportToolChat.java
@@ -14,22 +14,7 @@ public class SupportToolChat {
   public Response toolTest(JsonNode request) {
     var messages = (ArrayNode) request.get("messages");
     if (messages.size() >= 1) {
-      var current = messages.get(messages.size() - 1);
-      if (messages.size() == 2) {
-        if ("Help me, my computer is beeping, it started after opening AxonIvy Portal."
-            .equals(current.get("content").textValue())) {
-          return responder.send("response1.json");
-        }
-        if ("Computer is beeping after opening AxonIvy Portal. Need technical support."
-            .equals(current.get("content").textValue())) {
-          return responder.send("response2.json");
-        }
-      }
-
-      if (current.toString().contains("tool_call_id")
-          || "Computer is beeping after opening AxonIvy Portal".equals(current.get("content").textValue())) {
-        return responder.send("response3.json");
-      }
+      return responder.send("response1.json");
     }
     return Response.status(404).build();
   }
