bd976a70a05072502797245dbdc8b09e48cc2451
mnhnam-axonivy <nam.mai@axonactive.com>
2026-01-09 10:18:46 +0700

    AIAI-278 Prototype Guardrail Implementation - Added test for custom guardrail

--- smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/guardrails/TestGuardrailProvider.java
+++ smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/guardrails/TestGuardrailProvider.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.axonivy.utils.smart.workflow.guardrails.adapter.InputGuardrailAdapter;
+import com.axonivy.utils.smart.workflow.guardrails.dummy.DummyGuardrail;
 import com.axonivy.utils.smart.workflow.guardrails.input.PromptInjectionGuardrail;
 
 import ch.ivyteam.ivy.environment.AppFixture;
@@ -31,8 +32,10 @@ public class TestGuardrailProvider {
   @Test
   void providersListWithFilters() {
     var adapters = GuardrailProvider
-        .providersList(List.of("PromptInjectionGuardrail", "PromptInjectionGuardrail", "InvalidGuardrail"));
-    assertThat(adapters).hasSize(1); // duplicates and non-existed should be filtered
+        .providersList(
+            List.of("PromptInjectionGuardrail", "PromptInjectionGuardrail", "InvalidGuardrail", "DummyGuardrail"));
+    assertThat(adapters).hasSize(2); // duplicates and non-existed should be filtered
     assertThat(adapters.get(0).getDelegate()).isInstanceOf(PromptInjectionGuardrail.class);
+    assertThat(adapters.get(1).getDelegate()).isInstanceOf(DummyGuardrail.class);
   }
 }
