ff78b7f5c68534fb7bff2266e0c4d3cd0d63f431
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2026-04-07 16:47:49 +0200

    XIVY-18650 assert tool tracing attributes phoenix

---
+++ smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/tools/math/MathToolChat.java
@@ -0,0 +1,25 @@
+package com.axonivy.utils.smart.workflow.tools.math;
+
+import javax.ws.rs.core.Response;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import ch.ivyteam.test.resource.ResourceResponder;
+
+public class MathToolChat {
+
+  private final ResourceResponder responder = new ResourceResponder(MathToolChat.class);
+
+  public Response toolTest(JsonNode request) {
+    var messages = (ArrayNode) request.get("messages");
+    if (messages.size() == 2) {
+      return responder.send("r1ToolCall.json");
+    }
+    if (messages.size() == 4) {
+      return responder.send("r2Completed.json");
+    }
+    return Response.status(404).build();
+  }
+
+}
