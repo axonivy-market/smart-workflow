d7782d29c9d86a4686cedd4df705653fdc4f6453
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2026-04-23 11:31:54 +0200

    Fix #249: Identify smart-workflow PMV by LibraryID

--- smart-workflow/src/com/axonivy/utils/smart/workflow/spi/internal/SpiProject.java
+++ smart-workflow/src/com/axonivy/utils/smart/workflow/spi/internal/SpiProject.java
@@ -5,15 +5,23 @@ import java.util.function.Predicate;
 import ch.ivyteam.ivy.application.IProcessModelVersion;
 
 public final class SpiProject {
-  static String SMART_WORKFLOW_PROJECT = "smart-workflow";
+
+  private interface SmartWorkflow {
+    String GROUP_ID = "com.axonivy.utils.ai";
+    String ARTIFACT_ID = "smart-workflow";
+    String LIBRARY_ID = GROUP_ID + ":" + ARTIFACT_ID;
+  }
 
   public static IProcessModelVersion getSmartWorkflowPmv() {
-    Predicate<IProcessModelVersion> smartWorkflow = pmv -> SMART_WORKFLOW_PROJECT.equals(pmv.getName());
+    Predicate<IProcessModelVersion> smartWorkflow = pmv -> 
+      SmartWorkflow.LIBRARY_ID.equals(pmv.getLibraryId());
     var current = IProcessModelVersion.current();
     if (smartWorkflow.test(current)) {
       return current;
     }
     return current.getAllRequiredProcessModelVersions()
-        .filter(smartWorkflow).findAny().orElseThrow();
+        .filter(smartWorkflow)
+        .findAny()
+        .orElseThrow();
   }
 }
