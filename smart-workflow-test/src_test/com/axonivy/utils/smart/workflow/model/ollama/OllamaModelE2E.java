3c103ae68b5ec78369eb741226cf7c2d04f80cae
nhthinh <nht.8299@gmail.com>
2026-05-20 22:15:24 +0700

    Support-Ollama-as-an-AI-Model-Provider - Update E2E test to use testcontainers for Ollama

--- smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/model/ollama/OllamaModelE2E.java
+++ smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/model/ollama/OllamaModelE2E.java
@@ -2,10 +2,17 @@ package com.axonivy.utils.smart.workflow.model.ollama;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
+
 import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.ollama.OllamaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import com.axonivy.utils.smart.workflow.model.ChatModelFactory.AiConf;
 import com.axonivy.utils.smart.workflow.model.ollama.internal.OllamaServiceConnector.OllamaConf;
@@ -21,30 +28,42 @@ import ch.ivyteam.test.log.LoggerAccess;
 import dev.langchain4j.http.client.log.LoggingHttpClient;
 
 @IvyProcessTest
+@Testcontainers
 public class OllamaModelE2E {
 
+  private static final String MODEL_NAME = StringUtils.defaultIfBlank(
+      TestUtils.getSystemProperty("OLLAMA_MODEL"), "llama3.2");
+
   private static final BpmProcess AGENT_TOOLS = BpmProcess.name("TestToolUser");
+
+  @Container
+  private static final OllamaContainer ollama = new OllamaContainer(
+      DockerImageName.parse("ollama/ollama:latest"));
 
   @RegisterExtension
   LoggerAccess log = new LoggerAccess(LoggingHttpClient.class.getName());
 
+  @BeforeAll
+  static void pullModel() throws IOException, InterruptedException {
+    ollama.execInContainer("ollama", "pull", MODEL_NAME);
+  }
+
   @BeforeEach
   void setup(AppFixture fixture) {
     fixture.var(AiConf.DEFAULT_PROVIDER, OllamaModelProvider.NAME);
-    fixture.var(OllamaConf.BASE_URL,
-        StringUtils.defaultIfBlank(TestUtils.getSystemProperty("OLLAMA_BASE_URL"), "http://localhost:11434"));
-    fixture.var(OllamaConf.DEFAULT_MODEL,
-      StringUtils.defaultIfBlank(TestUtils.getSystemProperty("OLLAMA_MODEL"), "llama3.2"));
+    fixture.var(OllamaConf.BASE_URL, ollama.getEndpoint());
+    fixture.var(OllamaConf.DEFAULT_MODEL, MODEL_NAME);
   }
 
   @Test
-  void chatOutput_e2e(BpmClient client) {
+  void structuredOutput_e2e(BpmClient client) {
     Ivy.session().loginSessionUser("James", "secret");
     var res = client.start()
-        .process(AGENT_TOOLS.elementName("systemMessage"))
+        .process(AGENT_TOOLS.elementName("structuredOutput"))
         .as().session(Ivy.session())
         .execute();
     TestToolUserData data = res.data().last();
-    assertThat(StringUtils.isNotBlank(data.getResult())).isTrue();
+    assertThat(data.getPerson().getFirstName())
+        .isEqualTo("James");
   }
 }
