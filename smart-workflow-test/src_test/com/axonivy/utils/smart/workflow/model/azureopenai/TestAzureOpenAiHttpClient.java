0006fdb37a8997bb8540af58fec8d5fc91adb5c5
nhthinh-axonivy <thinh.nguyenhung@axonactive.com>
2026-06-02 19:18:35 +0700

    Azure-OpenAI-Provider-use-HTTP-Client - update test

--- smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/model/azureopenai/TestAzureOpenAiHttpClient.java
+++ smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/model/azureopenai/TestAzureOpenAiHttpClient.java
@@ -2,7 +2,6 @@ package com.axonivy.utils.smart.workflow.model.azureopenai;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.lang.reflect.Field;
 import java.time.Duration;
 
 import javax.ws.rs.client.WebTarget;
@@ -10,10 +9,8 @@ import javax.ws.rs.client.WebTarget;
 import org.junit.jupiter.api.Test;
 
 import com.axonivy.utils.smart.workflow.model.azureopenai.internal.AzureOpenAiServiceConnector;
-import com.axonivy.utils.smart.workflow.model.azureopenai.internal.client.SmartAzureHttpClient;
 import com.axonivy.utils.smart.workflow.model.azureopenai.internal.client.SmartAzureHttpClientProvider;
 
-import com.azure.core.http.HttpClient;
 import com.azure.core.util.HttpClientOptions;
 
 import ch.ivyteam.ivy.environment.IvyTest;
@@ -30,21 +27,18 @@ public class TestAzureOpenAiHttpClient {
   }
 
   @Test
-  void timeouts_areWiredOntoTheTarget() throws Exception {
+  void timeouts_areWiredOntoTheTarget() {
     var options = new HttpClientOptions()
         .setConnectTimeout(Duration.ofSeconds(5))
         .setReadTimeout(Duration.ofSeconds(7));
 
-    var config = targetOf(new SmartAzureHttpClientProvider().createInstance(options)).getConfiguration();
-
-    assertThat(config.getProperty("jersey.config.client.connectTimeout")).isEqualTo(5000L);
-    assertThat(config.getProperty("jersey.config.client.readTimeout")).isEqualTo(7000L);
-  }
-
-  private static WebTarget targetOf(HttpClient client) throws Exception {
-    Field field = SmartAzureHttpClient.class.getDeclaredField("target");
-    field.setAccessible(true);
-    return (WebTarget) field.get(client);
+    assertThat(new SmartAzureHttpClientProvider().createInstance(options))
+        .extracting("target")
+        .isInstanceOfSatisfying(WebTarget.class, target -> {
+          var config = target.getConfiguration();
+          assertThat(config.getProperty("jersey.config.client.connectTimeout")).isEqualTo(5000L);
+          assertThat(config.getProperty("jersey.config.client.readTimeout")).isEqualTo(7000L);
+        });
   }
 
 }
