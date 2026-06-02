f50e9b3a22f2d411f7252010dcad404facd4c035
nhthinh-axonivy <thinh.nguyenhung@axonactive.com>
2026-06-02 18:59:45 +0700

    Azure-OpenAI-Provider-use-HTTP-Client - Add HTTP client for Azure OpenAI provider - Refactor Azure OpenAI service connector to use the new HTTP client - Update tests to cover the new HTTP client implementation

---
+++ smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/model/azureopenai/TestAzureOpenAiHttpClient.java
@@ -0,0 +1,50 @@
+package com.axonivy.utils.smart.workflow.model.azureopenai;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Field;
+import java.time.Duration;
+
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.jupiter.api.Test;
+
+import com.axonivy.utils.smart.workflow.model.azureopenai.internal.AzureOpenAiServiceConnector;
+import com.axonivy.utils.smart.workflow.model.azureopenai.internal.client.SmartAzureHttpClient;
+import com.axonivy.utils.smart.workflow.model.azureopenai.internal.client.SmartAzureHttpClientProvider;
+
+import com.azure.core.http.HttpClient;
+import com.azure.core.util.HttpClientOptions;
+
+import ch.ivyteam.ivy.environment.IvyTest;
+
+@IvyTest
+public class TestAzureOpenAiHttpClient {
+
+  @Test
+  void smartIvyRestClient_isDefault() {
+    var builder = AzureOpenAiServiceConnector.buildOpenAiModel(null);
+    assertThat(builder).extracting("httpClientProvider")
+        .as("use ivy-jersey client; which integrates into our tracing tools")
+        .isInstanceOf(SmartAzureHttpClientProvider.class);
+  }
+
+  @Test
+  void timeouts_areWiredOntoTheTarget() throws Exception {
+    var options = new HttpClientOptions()
+        .setConnectTimeout(Duration.ofSeconds(5))
+        .setReadTimeout(Duration.ofSeconds(7));
+
+    var config = targetOf(new SmartAzureHttpClientProvider().createInstance(options)).getConfiguration();
+
+    assertThat(config.getProperty("jersey.config.client.connectTimeout")).isEqualTo(5000L);
+    assertThat(config.getProperty("jersey.config.client.readTimeout")).isEqualTo(7000L);
+  }
+
+  private static WebTarget targetOf(HttpClient client) throws Exception {
+    Field field = SmartAzureHttpClient.class.getDeclaredField("target");
+    field.setAccessible(true);
+    return (WebTarget) field.get(client);
+  }
+
+}
