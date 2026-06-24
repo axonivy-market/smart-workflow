b81ba114a51973f434f258f0ae39f8cf6237586e
mnhnam-axonivy <nam.mai@axonactive.com>
2026-06-24 11:06:17 +0700

    feat: add bean interfaces for workflow step dialogs

--- demo/smart-workflow-supplier-demo/src/com/axonivy/utils/smart/workflow/demo/supplier/onboarding/bean/interfaces/LogicCloseSupport.java
+++ demo/smart-workflow-supplier-demo/src/com/axonivy/utils/smart/workflow/demo/supplier/onboarding/bean/interfaces/LogicCloseSupport.java
@@ -2,19 +2,48 @@ package com.axonivy.utils.smart.workflow.demo.supplier.onboarding.bean.interface
 
 import javax.el.ELContext;
 import javax.el.MethodExpression;
-import javax.faces.application.Application;
 import javax.faces.context.FacesContext;
+
 
 public interface LogicCloseSupport {
 
   String LOGIC_CLOSE_EL = "#{logic.close}";
 
+  default void close() {
+    beforeClose();
+    callLogicClose();
+  }
+
+  default void close(Object argument) {
+    beforeClose();
+    callLogicClose(argument);
+  }
+
+  default void beforeClose() { }
+
+  default void callLogicClose() {
+    invokeLogicClose(new Class<?>[0]);
+  }
+
   default void callLogicClose(Object argument) {
+    invokeLogicClose(
+        new Class<?>[] { Object.class },
+        argument);
+  }
+
+  private void invokeLogicClose(Class<?>[] parameterTypes, Object... arguments) {
+
     FacesContext fc = FacesContext.getCurrentInstance();
     ELContext el = fc.getELContext();
-    Application app = fc.getApplication();
-    MethodExpression closeMethod = app.getExpressionFactory()
-        .createMethodExpression(el, LOGIC_CLOSE_EL, null, new Class<?>[] { Object.class });
-    closeMethod.invoke(el, new Object[] { argument });
+
+    MethodExpression closeMethod = fc.getApplication()
+        .getExpressionFactory()
+        .createMethodExpression(
+            el,
+            LOGIC_CLOSE_EL,
+            null,
+            parameterTypes);
+
+    closeMethod.invoke(el, arguments);
   }
 }
