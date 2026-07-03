2c7dd938852193ecdc7ff98bc789bab05cf47210
mnhnam-axonivy <nam.mai@axonactive.com>
2026-07-03 17:08:57 +0700

    fix(AIAI-387): remove unused import

--- smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/governance/history/analytic/TestHistoryAggregator.java
+++ smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/governance/history/analytic/TestHistoryAggregator.java
@@ -1,12 +1,10 @@
 package com.axonivy.utils.smart.workflow.governance.history.analytic;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
