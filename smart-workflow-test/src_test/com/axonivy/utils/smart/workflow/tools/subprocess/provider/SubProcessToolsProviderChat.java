f5c62f0d4bb0bceaa40e7a5b8a27af9d765391e1
mnhnam-axonivy <nam.mai@axonactive.com>
2026-07-14 08:18:45 +0700

    feat(AIAI-570): Convert smart-workflow-test and smart-workflow-webtest

--- smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/tools/subprocess/provider/SubProcessToolsProviderChat.java
+++ smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/tools/subprocess/provider/SubProcessToolsProviderChat.java
@@ -3,7 +3,7 @@ package com.axonivy.utils.smart.workflow.tools.subprocess.provider;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response;
 
 import org.apache.commons.io.IOUtils;
 
