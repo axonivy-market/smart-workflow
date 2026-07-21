4a9169598f634b6fe43bce08ac507e8c524d7006
mnhnam-axonivy <nam.mai@axonactive.com>
2026-07-21 10:36:52 +0700

    fix(AIAI-387): reverted unnecessary change

--- smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/model/TestChatModelProviders.java
+++ smart-workflow-test/src_test/com/axonivy/utils/smart/workflow/model/TestChatModelProviders.java
@@ -1,10 +1,11 @@
 package com.axonivy.utils.smart.workflow.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
