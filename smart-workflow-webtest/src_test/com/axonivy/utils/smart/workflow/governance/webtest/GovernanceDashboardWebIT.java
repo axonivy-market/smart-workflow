d8760bead770a331c0c489da8365e405c58b1968
mnhnam-axonivy <nam.mai@axonactive.com>
2026-07-24 18:17:39 +0700

    Try Chrome

--- smart-workflow-webtest/src_test/com/axonivy/utils/smart/workflow/governance/webtest/GovernanceDashboardWebIT.java
+++ smart-workflow-webtest/src_test/com/axonivy/utils/smart/workflow/governance/webtest/GovernanceDashboardWebIT.java
@@ -18,7 +18,7 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$$;
 import com.codeborne.selenide.SelenideElement;
 
-@IvyWebTest
+@IvyWebTest(browser="chrome")
 class GovernanceDashboardWebIT implements LoginFixture, GovernanceDashboardFixture {
 
   private GovernanceDashboardPage page;
