c1b6adf08b60a56239dfecc88d2641c545700d12
mnhnam-axonivy <nam.mai@axonactive.com>
2026-08-18 16:29:14 +0700

    fix: remove unused import libraries

--- smart-workflow/src/com/axonivy/utils/smart/workflow/guardrails/GuardrailCollector.java
+++ smart-workflow/src/com/axonivy/utils/smart/workflow/guardrails/GuardrailCollector.java
@@ -1,14 +1,11 @@
 package com.axonivy.utils.smart.workflow.guardrails;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
